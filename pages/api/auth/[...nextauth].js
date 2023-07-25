b4cf29cce71406dfdf55213f0a49415cbe95b697
xapski <xapski2671@gmail.com>
2023-07-25 16:20:44 +0100

    cm-03

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "@/lib/mongodb"
 
 export default NextAuth({
   providers: [
@@ -8,5 +10,6 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-  ]
+  ],
+  adapter: MongoDBAdapter(clientPromise, {databaseName:"ecomm"})
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,25 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
-	return <div>Hello World</div>
+	const { data: session } = useSession()
+	if (!session) {
+		return (
+			<div>
+				<div>
+					<button
+						onClick={() => {
+							signIn("google")
+						}}>
+						{"Log in with Google"}
+					</button>
+				</div>
+			</div>
+		)
+	}
+	return (
+		<div className="bg-blue-900 min-h-screen">
+			{"Logged in "}
+			{session.user?.email}
+		</div>
+	)
 }
